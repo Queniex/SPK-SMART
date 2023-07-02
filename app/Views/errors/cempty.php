4c6535d959579d269add72f76052b7c3285d9bf2
Queniex <Queniesalbiyah@gmail.com>
2023-07-02 15:39:23 +0700

    Setting error pages

---
+++ app/Views/errors/cempty.php
@@ -0,0 +1,10 @@
+<?= $this->extend('dashboard/templates/main'); ?>
+
+<?= $this->section('content'); ?>
+    <div class="flex justify-center h-screen bg-white items-center">
+        <div class="text-center mt-52 mb-64">
+            <center><img class="w-20 h-20" src="https://i.postimg.cc/LXfxw0MV/alert.gif" alt=""></center>
+            <h2 class="text-blue-500 font-semibold">Maaf Belum Terdapat Data Terpilih!</h2>
+        </div>
+    </div>
+<?= $this->endSection(); ?>
